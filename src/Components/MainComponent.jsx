74dd51531fd6acd822f0a3190a458907f4938349
vennhost <samogconsult@gmail.com>
2019-11-29 11:13:55 +0100

    first commit

---
+++ src/Components/MainComponent.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter, Route} from 'react-router-dom'
+
+class MainComponent extends React.Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+
+            </div>
+         );
+    }
+}
+ 
+export default MainComponent;
